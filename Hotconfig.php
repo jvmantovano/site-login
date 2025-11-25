7e6c30534d888ed50569eba8b5913aeeebc0e9c1
João Victor Mantovano <joaovictormantovano@gmail.com>
2025-11-24 21:01:27 -0300

    Create Hotconfig.php

---
+++ Hotconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbHost = 'sql100.infinityfree.com';
+$dbUsername = 'if0_40490679';
+$dbPassword = '';
+$dbName = 'if0_40490679_joao';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
